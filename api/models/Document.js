884741da51d3fd534acb0be1898a27719911e754
Phan Đức Anh <94965115+Pdanh1811@users.noreply.github.com>
2022-04-21 01:33:28 +0700

    Add files via upload

---
+++ api/models/Document.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const DocumentSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, unique: true },
+    desc: { type: String, require: true },
+    file: { type: String, require: true },
+    year: { type: String },
+    limit: { type: Number },
+    classify: { type: String, require: true },
+    isSeries: { type: Boolean, default: false },
+    uploadby: { type: String, default: "anonymous"},
+    author: { type: String, require: true}
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Document", DocumentSchema);
---
+++ api/routes/verifyToken.js
@@ -0,0 +1,48 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const Document = require("../models/Document")
+
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+      if (err) res.status(403).json("Token is not valid!");
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(401).json("You are not authenticated!");
+  }
+};
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, async () => {
+    FindUser = await User.findOne({ _id: req.user.id });
+    if (FindUser !== null || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not alowed to do that!");
+    }
+  });
+};
+
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not admin to do that!");
+    }
+  });
+};
+
+
+
+
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin, 
+};
